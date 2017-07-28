607d552b142daecb6be1f20b0f537d65b7c2e80b
patrickhrastnik <30524365+patrickhrastnik@users.noreply.github.com>
2017-07-28 13:30:45 +0200

    Missing AMD Module name
    This source file was missing an AMD module name. Other parts of the DataTables source collection (e.g. datatables.net-fixedheader) require module 'datatables.net'.

--- js/jquery.dataTables.js
+++ js/jquery.dataTables.js
@@ -29,7 +29,7 @@
 
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( ['jquery'], function ( $ ) {
+		define('datatables.net', ['jquery'], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
