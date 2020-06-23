d5e876955f53e469c4353f026f93eb569eb76c56
Allan Jardine <allan.jardine@sprymedia.co.uk>
2020-06-23 15:10:41 +0100

    Added polyfill
    Sync to source repo @339810be48205c3c61e10a5603a93d4399e32bc2

--- js/jquery.dataTables.js
+++ js/jquery.dataTables.js
@@ -1584,6 +1584,13 @@
 		return out;
 	};
 	
+	// Array.isArray polyfill.
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+	if (! Array.isArray) {
+	    Array.isArray = function(arg) {
+	        return Object.prototype.toString.call(arg) === '[object Array]';
+	    };
+	}
 	
 	/**
 	 * DataTables utility methods
