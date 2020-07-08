abb3abbb4eb6b4e08b7604a92102a02a7f96bba7
Allan Jardine <allan.jardine@sprymedia.co.uk>
2020-07-08 10:26:09 +0100

    Added .trim() polyfill
    Sync to source repo @76e1a927f8de455a708af8e61d002d08538855f4

--- js/jquery.dataTables.js
+++ js/jquery.dataTables.js
@@ -1592,6 +1592,14 @@
 	    };
 	}
 	
+	// .trim() polyfill
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+	if (!String.prototype.trim) {
+	  String.prototype.trim = function () {
+	    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+	  };
+	}
+	
 	/**
 	 * DataTables utility methods
 	 * 
