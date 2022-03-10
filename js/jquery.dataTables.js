ed857e714247da80c052028a29cc94d2d3ef8739
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-03-10 15:36:28 +0000

    caef2f86e0c9a428fe5b840b1333f8e649462680 fix: Fix issue with page length select element not being set when a state is reloaded
    Jira Issue DD-2489
    
    58769570d567f4615b555c3e19edc8fc8b0f4bd2 Merge branch 'master' of github.com:DataTables/DataTablesSrc
    
    Sync to source repo @58769570d567f4615b555c3e19edc8fc8b0f4bd2

--- js/jquery.dataTables.js
+++ js/jquery.dataTables.js
@@ -6472,7 +6472,13 @@
 			}
 		}
 		if ( s.length !== undefined ) {
-			settings._iDisplayLength   = s.length;
+			// If already initialised just set the value directly so that the select element is also updated
+			if (api) {
+				api.page.len(s.length)
+			}
+			else {
+				settings._iDisplayLength   = s.length;
+			}
 		}
 	
 		// Order
