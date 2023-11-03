5c93272b1b08fa63a613ca8be7684bfcfd7b7653
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-11-03 14:30:32 +0000

    4b83774b58ed7c423c18356e1df0963513df6f2f Release 1.13.7
    Sync to source repo @4b83774b58ed7c423c18356e1df0963513df6f2f

--- js/jquery.dataTables.mjs
+++ js/jquery.dataTables.mjs
@@ -1,4 +1,4 @@
-/*! DataTables 1.13.6
+/*! DataTables 1.13.7
  * ©2008-2023 SpryMedia Ltd - datatables.net/license
  */
 
@@ -9738,7 +9738,7 @@ _api_register( 'i18n()', function ( token, def, plural ) {
  *  @type string
  *  @default Version number
  */
-DataTable.version = "1.13.6";
+DataTable.version = "1.13.7";
 
 /**
  * Private data store, containing all of the settings objects that are
