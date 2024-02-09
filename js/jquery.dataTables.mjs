92d424dce447f22d39c0c901da2ff9f98d626bbb
Allan Jardine <allan@datatables.net>
2024-02-09 16:33:50 +0000

    Release 1.13.10

--- js/jquery.dataTables.mjs
+++ js/jquery.dataTables.mjs
@@ -1,4 +1,4 @@
-/*! DataTables 1.13.9
+/*! DataTables 1.13.10
  * ©2008-2024 SpryMedia Ltd - datatables.net/license
  */
 
@@ -9744,7 +9744,7 @@ _api_register( 'i18n()', function ( token, def, plural ) {
  *  @type string
  *  @default Version number
  */
-DataTable.version = "1.13.9";
+DataTable.version = "1.13.10";
 
 /**
  * Private data store, containing all of the settings objects that are
