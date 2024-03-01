fd77e638139c5e7c4339d7e7494294f458b50704
dtbuild <dtbuild@rigil>
2024-03-01 10:36:24 +0100

    f56b29ff93c334811d0292b64c9b696954a16fd1 Fix: `-api table().footer.structure()` would give `undefined` for a footer's title if the title span had been overwritten
    https://datatables.net/forums/discussion/78352
    
    Sync to source repo @f56b29ff93c334811d0292b64c9b696954a16fd1

--- js/dataTables.js
+++ js/dataTables.js
@@ -3266,11 +3266,15 @@
 						colspan++;
 					}
 	
+					var titleSpan = $('span.dt-column-title', cell);
+	
 					structure[row][column] = {
 						cell: cell,
 						colspan: colspan,
 						rowspan: rowspan,
-						title: $('span.dt-column-title', cell).html()
+						title: titleSpan.length
+							? titleSpan.html()
+							: $(cell).html()
 					};
 				}
 			}
