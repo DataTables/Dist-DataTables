617208405bc6abf791d8a17ee317e6cdc3976225
dtbuild <dtbuild@rigil>
2024-04-09 15:15:22 +0000

    ded438d92e3b54bc95c1a2dabbaf55e372dee4bf Fix: Update HTML stripping to use non-polynomial regex
    Sync to source repo @ded438d92e3b54bc95c1a2dabbaf55e372dee4bf

--- js/dataTables.js
+++ js/dataTables.js
@@ -1096,7 +1096,7 @@
 	
 	var _re_dic = {};
 	var _re_new_lines = /[\r\n\u2028]/g;
-	var _re_html = /<.*?>/g;
+	var _re_html = /<[^>]*?>/g;
 	
 	// This is not strict ISO8601 - Date.parse() is quite lax, although
 	// implementations differ between browsers.
