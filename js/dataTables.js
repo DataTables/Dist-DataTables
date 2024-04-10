cf028fbe357b157b7befd4dca01956ab4d6c797f
dtbuild <dtbuild@rigil>
2024-04-10 08:35:22 +0000

    6944c25bde6715119e96a47c7b3b4d2010110dde Dev: Move regex into html removal function
    Looks like CodeQL might prefer this?!
    
    Sync to source repo @6944c25bde6715119e96a47c7b3b4d2010110dde

--- js/dataTables.js
+++ js/dataTables.js
@@ -1287,7 +1287,7 @@
 	var _stripHtml = function (input) {
 		var previous;
 	
-		input = input.replace(_re_html, ''); // Complete tags
+		input = input.replace(/<[^>]*>/g, ''); // Complete tags
 	
 		// Safety for incomplete script tag - use do / while to ensure that
 		// we get all instances
