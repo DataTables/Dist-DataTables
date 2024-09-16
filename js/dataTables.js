1cb130d197ff24fc90a6fc1f8891abe6dae0c191
dtbuild <dtbuild@rigil>
2024-09-16 14:40:25 +0000

    f181926d2ebcb27e3b8004964235d37891778950 Fix: Safari 9 doesn't support `String.prototype.normalize`
    Sync to source repo @f181926d2ebcb27e3b8004964235d37891778950

--- js/dataTables.js
+++ js/dataTables.js
@@ -1318,8 +1318,11 @@
 		}
 	
 		// It is faster to just run `normalize` than it is to check if
-		// we need to with a regex!
-		var res = str.normalize("NFD");
+		// we need to with a regex! (Check as it isn't available in old
+		// Safari)
+		var res = str.normalize
+			? str.normalize("NFD")
+			: str;
 	
 		// Equally, here we check if a regex is needed or not
 		return res.length !== str.length
