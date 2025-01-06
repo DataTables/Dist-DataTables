afabc89c977bf25971ae20c268c75ffaaefd7d87
dtbuild <dtbuild@rigil>
2025-01-06 12:15:23 +0000

    8e155b3eca39175eec7e9c1fe3ecb273e63a438b Release 2.2.0
    (Removed one of the UMD headers to make it the same as the ESM build -
    it was redundant information)
    
    Sync to source repo @8e155b3eca39175eec7e9c1fe3ecb273e63a438b

--- js/dataTables.mjs
+++ js/dataTables.mjs
@@ -1,4 +1,4 @@
-/*! DataTables 2.2.0-dev
+/*! DataTables 2.2.0
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -9949,7 +9949,7 @@ _api_register( 'i18n()', function ( token, def, plural ) {
  *  @type string
  *  @default Version number
  */
-DataTable.version = "2.2.0-dev";
+DataTable.version = "2.2.0";
 
 /**
  * Private data store, containing all of the settings objects that are
