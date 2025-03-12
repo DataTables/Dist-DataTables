c691a89d9bd2d1f10978e422dc309d21ab72802f
dtbuild <dtbuild@rigil>
2025-03-12 12:15:26 +0000

    40cf6acad407d0c37e9fb55f96f92ae94380ce6e New: `-init ajax.submitAs` option to control if data should be submitted as HTTP parameters (default), or a JSON string.
    This replaces `ajax.data` being used as a function and returning a
    string (which is still possible, but not recommended and won't work with
    SearchPanes and others).
    
    https://datatables.net/forums/discussion/80705
    
    Sync to source repo @40cf6acad407d0c37e9fb55f96f92ae94380ce6e

--- js/dataTables.js
+++ js/dataTables.js
@@ -4176,6 +4176,11 @@
 		// Allow plug-ins and external processes to modify the data
 		_fnCallbackFire( oSettings, null, 'preXhr', [oSettings, data, baseAjax], true );
 	
+		// Custom Ajax option to submit the parameters as a JSON string
+		if (baseAjax.submitAs === 'json' && typeof data === 'object') {
+			baseAjax.data = JSON.stringify(data);
+		}
+	
 		if ( typeof ajax === 'function' )
 		{
 			// Is a function - let the caller define what needs to be done
