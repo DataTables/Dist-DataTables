d51ac350d033f4597b2e66b21dfaa46e46424ea1
dtbuild <dtbuild@rigil>
2025-12-23 12:30:20 +0000

    b10fe2c01fe4f23b89984419d769cb44ae5482b3 Release 2.3.6
    Sync to source repo @b10fe2c01fe4f23b89984419d769cb44ae5482b3

--- js/dataTables.js
+++ js/dataTables.js
@@ -1,4 +1,4 @@
-/*! DataTables 2.3.5
+/*! DataTables 2.3.6
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -10288,7 +10288,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "2.3.5";
+	DataTable.version = "2.3.6";
 	
 	/**
 	 * Private data store, containing all of the settings objects that are
