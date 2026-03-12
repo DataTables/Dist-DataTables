ee6310aa16b57a083af71643fb8b85dc576606cb
dtbuild <dtbuild@rigil>
2026-03-12 16:30:21 +0000

    6abc22f100fc712bd058e1ab08c6664e12f9a831 Fix: Don't include `<template>` tags in the sizing table
    https://github.com/DataTables/DataTablesSrc/commit/9f7d24c6c133c1132854487eeefb65d22240813d
    https://github.com/DataTables/DataTablesSrc/issues/376#issuecomment-4025042005
    
    Sync to source repo @6abc22f100fc712bd058e1ab08c6664e12f9a831

--- js/dataTables.js
+++ js/dataTables.js
@@ -5720,11 +5720,12 @@
 					.replace(/id=".*?"/g, '')
 					.replace(/name=".*?"/g, '');
 	
-				// Don't want script or dialog tags in the width calculations as
-				// they are hidden content
+				// Don't want script, dialog or template tags in the width
+				// calculations as they are hidden content
 				cellString = cellString
 					.replace(/<script.*?<\/script>/gi, ' ')
-					.replace(/<dialog.*?<\/dialog>/gi, ' ');
+					.replace(/<dialog.*?<\/dialog>/gi, ' ')
+					.replace(/<template.*?<\/template>/gi, ' ');
 	
 				var noHtml = _stripHtml(cellString, ' ')
 					.replace( /&nbsp;/g, ' ' );
