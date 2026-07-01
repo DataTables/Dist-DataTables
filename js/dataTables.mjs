af637e302b33342be3720ce8ea6459cde27d3e22
dtbuild <dtbuild@rigil>
2026-07-01 14:40:21 +0000

    cf1afdd07568c579ea9bb6859ad4ad9dda02192a Dev: Tweak for the `initDraw` event - check the column types first
    This is a small change in that it will check the column types regardless
    of the flags for the draw, which I am okay with.
    
    Sync to source repo @cf1afdd07568c579ea9bb6859ad4ad9dda02192a

--- js/dataTables.mjs
+++ js/dataTables.mjs
@@ -8379,12 +8379,11 @@ function draw(settings, ajaxComplete) {
  */
 function reDraw(settings, holdPosition, recompute) {
     let features = settings.features, doSort = features.ordering, doFilter = features.searching;
+    // Resolve column types
+    columnTypes(settings);
     // Announce that we are going to do the draw
     callbackFire(settings, null, 'initDraw', [settings], false);
     if (recompute === undefined || recompute === true) {
-        // Resolve any column types that are unknown due to addition or
-        // invalidation
-        columnTypes(settings);
         if (doSort) {
             sort(settings);
         }
